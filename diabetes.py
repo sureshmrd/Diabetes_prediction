f2b16438c3abd0e3624adb088ab897c85351bbc6
suresh <123853377+sureshmrd@users.noreply.github.com>
2023-06-21 17:56:31 +0530

    diabetes.py
    changes the path

--- diabetes.py
+++ diabetes.py
@@ -12,7 +12,7 @@ import streamlit as st
 
 #loading trained saved model
 
-loaded_model=pickle.load(open('C:/Users/Suresh/trained_model.sav','rb'))
+loaded_model=pickle.load(open('trained_model.sav','rb'))
 
 #creating prediction
 def diabetics_pred(input_data):
